e1fe26a3c958214c03f3c45ab8c986ec54d27cb3
Ben <ben.smith@robomq.io>
2018-09-29 19:57:38 -0400

    Updated the test script

--- modules/base/scripts/testCoreJS.sh
+++ modules/base/scripts/testCoreJS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-./node_modules/.bin/mocha --recursive
-./node_modules/.bin/eslint ./test
-./node_modules/.bin/eslint ./lib
-./node_modules/.bin/eslint ./ble.js
+mocha --recursive
+eslint ./test
+eslint ./lib
+eslint ./ble.js
